e34fd534bdbeab29684f88470ad82de953ddc0d7
Siwar Chabbi <siwarchabbi7@gmail.com>
2023-12-29 17:22:12 +0100

    test

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-
 require('dotenv').config();
 const express = require("express");
 const mongoose = require('mongoose');
 const connectDb = require("./config/dbConnection");
 const errorHandler = require("./middleware/errorHandler");
+const cors = require('cors');
 
 mongoose.set('strictQuery', false);
 
@@ -14,8 +14,16 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+// Configure CORS to allow requests from http://localhost:4200
+app.use(cors({
+  origin: 'http://localhost:4200',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
 app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/car" , require("./routes/carRoutes"));
+app.use("/api/car", require("./routes/carRoutes"));
 
 app.use(errorHandler);
 
