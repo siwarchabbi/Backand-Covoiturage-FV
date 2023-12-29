032d288ee1b38bcc9ae3cf71fa6b42f675ec78db
Siwar Chabbi <siwarchabbi7@gmail.com>
2023-12-29 18:39:38 +0100

    Add Cros Express API !

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const express = require("express");
+const express = require('express');
 const mongoose = require('mongoose');
-const connectDb = require("./config/dbConnection");
-const errorHandler = require("./middleware/errorHandler");
+const connectDb = require('./config/dbConnection');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors'); // Import the cors middleware
 
 mongoose.set('strictQuery', false);
 
@@ -11,11 +12,12 @@ connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors()); // Use cors middleware to enable Cross-Origin Resource Sharing
 app.use(express.json());
 
-app.use("/api/comments", require("./routes/commentRoutes"));
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/car", require("./routes/carRoutes"));
+app.use('/api/comments', require('./routes/commentRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/car', require('./routes/carRoutes'));
 
 app.use(errorHandler);
 
